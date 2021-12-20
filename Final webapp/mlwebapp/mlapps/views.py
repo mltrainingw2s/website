a942f83a397e98b75b00cb3e59dbdf032061c49d
mltrainingw2s <vignesh.dhan@way2smile.com>
2021-12-20 15:58:41 +0530

    latest update 4pm dec 20

--- Final webapp/mlwebapp/mlapps/views.py
+++ Final webapp/mlwebapp/mlapps/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def imgdetect(request):
+    return render(request,"imagedetect.html")
+
+def videodetect(request):
+    return render(request,"webcamdetect.html")
--- Final webapp/mlwebapp/mlwebapp/urls.py
+++ Final webapp/mlwebapp/mlwebapp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from mlapps import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index, name="index"),
+    path('image/',views.imgdetect, name="image"),
+    path('webcam/',views.videodetect, name="webcam"),
 ]
