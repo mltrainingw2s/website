80fef5ae1beaf429277018d7f1d9a8080554acd4
mltrainingw2s <vignesh.dhan@way2smile.com>
2021-12-28 18:37:09 +0530

    checking for deploy

--- Final webapp/mlwebapp/mlapps/urls.py
+++ Final webapp/mlwebapp/mlapps/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .mlapps import views
 urlpatterns = [
     path('',views.index, name="index"),
-    path('image/',views.imgdetect, name="image"),
-    path('webcam/',views.videodetect, name="webcam"),
-    path('video_feed/', views.video_feed, name='video_feed'),
-    path('snap_feed/',views.snaps().snap_feed,name='snap_feed'),
-    path('snapcam/',views.snaps().snapdetect,name='snap_detect'),
+    # path('image/',views.imgdetect, name="image"),
+    # path('webcam/',views.videodetect, name="webcam"),
+    # path('video_feed/', views.video_feed, name='video_feed'),
+    # path('snap_feed/',views.snaps().snap_feed,name='snap_feed'),
+    # path('snapcam/',views.snaps().snapdetect,name='snap_detect'),
 ]
