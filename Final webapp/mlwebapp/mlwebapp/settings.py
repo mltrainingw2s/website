97e8f98b3e5b3886b5bb13a3b03b9edffe58efde
mltrainingw2s <vignesh.dhan@way2smile.com>
2021-12-28 19:06:11 +0530

    checking for deploy

--- Final webapp/mlwebapp/mlwebapp/settings.py
+++ Final webapp/mlwebapp/mlwebapp/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mlapps',
-        'HOST': '13.234.21.152',
+        'HOST': 'localhost',
         'USER':'root',
         'PASSWORD':''
         }
