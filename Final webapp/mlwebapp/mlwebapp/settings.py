767f0da28a4828d7e05b8d1ae61f663086eee485
mltrainingw2s <vignesh.dhan@way2smile.com>
2021-12-28 18:40:49 +0530

    checking for deploy

--- Final webapp/mlwebapp/mlwebapp/settings.py
+++ Final webapp/mlwebapp/mlwebapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l#(af+ze*e(_fqv#qq%3_$z-ifqyujed6j0f6p%^wn6z@6iuz9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.234.21.152']
 
 
 # Application definition
