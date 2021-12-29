03b92de6a9575781753cb50765c16e54ae1da675
anbu13051998 <anbu.arul@way2smile.com>
2021-12-29 13:28:23 +0530

    test model

--- Final webapp/mlwebapp/mlapps/models.py
+++ Final webapp/mlwebapp/mlapps/models.py
@@ -1,25 +1,25 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
-# from django.db import models
+# Create your models here.
+from django.db import models
 
-# # Create your models here.
+# Create your models here.
 
-# class Ml_Image(models.Model):
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     image_upload = models.CharField(max_length=250,null = True)
-#     smile_percentage = models.IntegerField(null = True)
-#     image_type = models.IntegerField(null = True)
-#     class Meta:
-#         db_table = 'Image'
+class Ml_Image(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image_upload = models.CharField(max_length=250,null = True)
+    smile_percentage = models.IntegerField(null = True)
+    image_type = models.IntegerField(null = True)
+    class Meta:
+        db_table = 'Image'
 
-# class Ml_web(models.Model):
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now = True)
-#     image_upload = models.CharField(max_length = 250, null = True)
-#     image_type = models.IntegerField(null = True)
-#     smile_percentage = models.IntegerField(null = True)
-#     class Meta:
-#         db_table = 'WebCam'
+class Ml_web(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+    image_upload = models.CharField(max_length = 250, null = True)
+    image_type = models.IntegerField(null = True)
+    smile_percentage = models.IntegerField(null = True)
+    class Meta:
+        db_table = 'WebCam'
 
