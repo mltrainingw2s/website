de253bd63e3f7e715303266e33978c24cd5430fe
mltrainingw2s <vignesh.dhan@way2smile.com>
2022-01-25 12:59:47 +0530

    changed views

--- Final webapp/mlwebapp/restapi/views.py
+++ Final webapp/mlwebapp/restapi/views.py
@@ -39,6 +39,7 @@ class Image_detect(APIView):
         return Response({'gallery':serializer.data})
 
     def post(self,request):
+        print("------------------",request.body.loads)
         if 'data' in request.FILES:
             imgs = request.FILES['data']
             a=str(imgs).split('.')
